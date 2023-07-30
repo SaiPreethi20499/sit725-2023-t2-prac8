53fd34c5dd5d7618277ea7bdf2143ec45b37c0c0
G. Preethi <preethisai597@gmail.com>
2023-07-30 20:06:20 +1000

    Creating a web application
    This is a demo application that tries to use a few components from materialize.

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const cardList = [{
+    title: 'Mumbai',
+    path: 'images/Mumbai.jpg',
+    subTitle: 'About Mumbai',
+    description: 'Description of Mumbai'
+},
+{
+    title: 'Hyderabad',
+    path: 'images/Charminar1.jpg',
+    subTitle: 'About Hyderabad',
+    description: 'Description of Hyderabad'
+}];
+
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = `<div class="col s4 center-align">
+            <div class="card medium">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator card-image img" src="${item.path}">
+                </div>
+                <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+                    <p><a href="#">${item.subTitle}</a></p>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+                    <p class="card-text">${item.description}</p>
+                </div>
+            </div>
+        </div>`;
+        $("#card-section").append(itemToAppend);
+    });
+};
+
+
+const formSumitted = () => {
+    let formData = {};
+    formData.firstName = $('#first_name').val();
+    formData.lastName = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ",formData);
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('#formSubmit').click(()=>{
+        formSumitted();
+    });
+    addCards(cardList);
+});
