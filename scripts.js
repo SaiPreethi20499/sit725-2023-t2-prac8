fccf53a20cf2eb0f8acb371015a8f95e5ceb0859
G. Preethi <preethisai597@gmail.com>
2023-08-08 15:05:13 +1000

    Add database to server
    Give input in the front end and get output from the back end.

--- scripts.js
+++ scripts.js
@@ -1,54 +1,54 @@
-const cardList = [{
-    title: 'Mumbai',
-    path: 'images/Mumbai.jpg',
-    subTitle: 'About Mumbai',
-    description: 'Description of Mumbai'
-},
-{
-    title: 'Hyderabad',
-    path: 'images/Charminar1.jpg',
-    subTitle: 'About Hyderabad',
-    description: 'Description of Hyderabad'
-}];
-
-
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = `<div class="col s4 center-align">
-            <div class="card medium">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator card-image img" src="${item.path}">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
-                    <p><a href="#">${item.subTitle}</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
-                    <p class="card-text">${item.description}</p>
-                </div>
-            </div>
-        </div>`;
-        $("#card-section").append(itemToAppend);
+        let itemToAppend = '<div class="col s4 center-align">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
+                '</div><div class="card-content">'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.subTitle+'</a></p></div>'+
+                '<div class="card-reveal">'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
+                '</div></div></div>';
+        $("#card-section").append(itemToAppend)
     });
-};
-
+}
 
 const formSumitted = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
 
-    console.log("Form Data Submitted: ",formData);
+    console.log(formData);
+    postCity(formData);
+}
+
+function postCity(city){
+    $.ajax({
+        url:'/api/city',
+        type:'POST',
+        data:city,
+        success: (result)=>{
+            if (result.statusCode === 201) {
+                alert('City added');
+            }
+        }
+    });
+}
+
+function getAllCities(){
+    $.get('/api/cities', (result) => {
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    });
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('.modal').modal();
     $('#formSubmit').click(()=>{
         formSumitted();
     });
-    addCards(cardList);
+    $('.modal').modal();
+    getAllCities();
 });
